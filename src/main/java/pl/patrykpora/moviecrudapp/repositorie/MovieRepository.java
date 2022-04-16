a9884344e4aaaa4689cab004b368ada8446936e9
unknown <patryk_pora@o2.pl>
2022-04-16 19:05:39 +0200

    getByid and create movie

--- src/main/java/pl/patrykpora/moviecrudapp/repositorie/MovieRepository.java
+++ src/main/java/pl/patrykpora/moviecrudapp/repositorie/MovieRepository.java
@@ -20,4 +20,14 @@ public class MovieRepository {
                 BeanPropertyRowMapper.newInstance(Movie.class));
     }
 
+    public Movie getById(int id) {
+        return jdbcTemplate.queryForObject("SELECT id, name, rating FROM movie WHERE id = ?",
+                BeanPropertyRowMapper.newInstance(Movie.class), id);
+    }
+
+    public int save(List<Movie> movies) {
+        movies.forEach(movie -> jdbcTemplate.update("INSERT INTO movie(name, rating) VALUES(?, ?)",
+                movie.getName(), movie.getRating()));
+        return 1;
+    }
 }
